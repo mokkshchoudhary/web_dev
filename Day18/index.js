25664687815317213ac4552f3d204a80730bea05
Moksh Choudhary <mokkshchoudhary@gmail.com>
2025-04-05 02:16:21 +0530

    Add files via upload

---
+++ Day18/index.js
@@ -0,0 +1,50 @@
+//console.log(`Hello`);
+//console.log(`I like pizza!`);
+
+
+//window.alert(`This is an alert!`)
+
+
+//document.getElementById("myh1").textContent = `Hello`;
+//document.getElementById("myp").textContent=`I like pizza!`;
+// This is a comment
+
+//variables
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+//strings
+let firstname = "Kakarot";
+let lastname = "Goku";
+
+//booleans
+
+let online = true;
+
+console.log(age);
+console.log(price);
+console.log(gpa);
+console.log(`you are ${age}  years old`)
+console.log(`the price is ${price}`)
+console.log(`your gpa is ${gpa}`)
+console.log(typeof gpa);
+console.log(typeof firstname);
+console.log(`my name is ${firstname} ${lastname}`);
+console.log(`bro is online: ${online}`);
+
+document.getElementById("p1").textContent = `my name is ${firstname}`;
+document.getElementById("p2").textContent = `my age is ${age}`;
+document.getElementById("p3").textContent = `and i have a gpa ${gpa}`
+
+
+//arithmatic operators 
+
+let students= 30;
+//students = students+1
+//students = students-1
+//students = students **2
+//students = students *2
+//students = students /2
+
+console.log(students)
